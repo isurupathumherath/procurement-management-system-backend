a3424085bb3744ad42c926e7940ba2503c576953
Ahamed <fahamed@wiley.com>
2022-11-01 00:03:02 +0530

    🚨latest fix

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,13 @@ import express from "express";
 import userRouter from "./user.route.js";
 import supplierRouter from "./supplier.route.js";
 import orderRouter from "./order.route.js";
+import orderNewRouter from "./orderNew.route.js";
 
 const apiRouter = express.Router();
 
 apiRouter.use("/user", userRouter);
 apiRouter.use("/supplier", supplierRouter);
 apiRouter.use("/order", orderRouter);
+apiRouter.use("/orderNew", orderNewRouter);
 
 export default apiRouter;
