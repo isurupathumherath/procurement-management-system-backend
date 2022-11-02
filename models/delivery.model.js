edef4f9ca0a8f72989e96e8770c0578f52df1bcd
Ahamed <fahamed@wiley.com>
2022-11-02 22:24:39 +0530

    delivery model fix

--- models/delivery.model.js
+++ models/delivery.model.js
@@ -31,9 +31,14 @@ const OrderNewSchema = new Schema({
     default: "Supplier didn't added",
   },
   deliveryAddress: String,
+
   deliveredDate: {
     type: String,
     default: "date did not added",
+  },
+  rejectionNote: {
+    type: String,
+    default: "Order didn't rejected",
   },
   isReceiptPrinted: {
     type: Boolean,
