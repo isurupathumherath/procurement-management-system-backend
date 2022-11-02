1f78e7df3de296841c1c666e6f415c795aa2f269
Ahamed <fahamed@wiley.com>
2022-11-03 01:06:57 +0530

    Product service added

--- services/index.js
+++ services/index.js
@@ -2,3 +2,4 @@ export * from "./user.service.js";
 export * from "./supplier.service.js";
 export * from "./order.service.js";
 export * from "./delivery.service.js";
+export * from "./product.service.js";
---
+++ services/product.service.js
@@ -0,0 +1,71 @@
+import {
+  createProduct,
+  getProductforSupplier,
+  updateProduct,
+  getAllProducts,
+  deleteProduct,
+} from "../repository/index.js";
+import AppError from "../utils/appError.js";
+
+export const saveProductService = async (data, supplierID) => {
+  const {
+    itemName,
+    itemBrand,
+    image,
+    availableQty,
+    description,
+    measuringUnit,
+    price,
+  } = data;
+  try {
+    const product = await createProduct({
+      itemName,
+      itemBrand,
+      image,
+      availableQty,
+      description,
+      measuringUnit,
+      price,
+      supplierID,
+    });
+    return Promise.resolve(product);
+  } catch (err) {
+    throw new AppError(err.message, err.status);
+  }
+};
+
+export const updateProductService = async (id, data) => {
+  try {
+    const product = await updateProduct(id, data);
+    return Promise.resolve(product);
+  } catch (err) {
+    throw new AppError(err.message, err.status);
+  }
+};
+
+export const deleteProductService = async (id) => {
+  try {
+    const product = await deleteProduct(id);
+    return Promise.resolve(product);
+  } catch (err) {
+    throw new AppError(err.message, err.status);
+  }
+};
+
+export const getAllProductService = async () => {
+  try {
+    const products = await getAllProducts();
+    return Promise.resolve(products);
+  } catch (err) {
+    throw new AppError(err.message, err.status);
+  }
+};
+
+export const getProductForSupplierService = async (id) => {
+  try {
+    const products = await getProductforSupplier(id);
+    return Promise.resolve(products);
+  } catch (err) {
+    throw new AppError(err.message, err.status);
+  }
+};
