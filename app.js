90486759eb10f1f6614be82b62ce13aa8805baaa
Ahamed <fahamed@wiley.com>
2022-11-06 15:58:43 +0530

    model fix

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ import apiRouter from "./routes/index.js";
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(express.json({ limit: "50mb", extended: true }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
 
 connect();
 
